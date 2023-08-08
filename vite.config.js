d49c0f9f8f215dfd7ba855e1a2fd3a6819693d2e
yuhsuanlii <charlie9684@gmail.com>
2023-08-09 01:49:50 +0800

    feat: gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 開發中、產品路徑 分開設定
+  // eslint-disable-next-line no-undef
+  base: process.env.NODE_ENV === 'production' ? '/react-gh-pages-sample/' : '/',
   plugins: [react()],
 })
