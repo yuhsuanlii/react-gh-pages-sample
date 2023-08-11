6f859a51986d4479a4c56c4b7235181e8e79e99e
yuhsuanlii <charlie9684@gmail.com>
2023-08-12 02:21:06 +0800

    first week

---
+++ src/homework/First.jsx
@@ -0,0 +1,122 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react"
+
+const data = [
+    {
+        id: 1,
+        name: "珍珠奶茶",
+        description: "香濃奶茶搭配QQ珍珠",
+        price: 50,
+        quantity: 20
+    },
+    {
+        id: 2,
+        name: "冬瓜檸檬",
+        description: "清新冬瓜配上新鮮檸檬",
+        price: 45,
+        quantity: 18
+    },
+    {
+        id: 3,
+        name: "翡翠檸檬",
+        description: "綠茶與檸檬的完美結合",
+        price: 55,
+        quantity: 34
+    },
+    {
+        id: 4,
+        name: "四季春茶",
+        description: "香醇四季春茶，回甘無比",
+        price: 45,
+        quantity: 10
+    },
+    {
+        id: 5,
+        name: "阿薩姆奶茶",
+        description: "阿薩姆紅茶搭配香醇鮮奶",
+        price: 50,
+        quantity: 25
+    },
+    {
+        id: 6,
+        name: "檸檬冰茶",
+        description: "檸檬與冰茶的清新組合",
+        price: 45,
+        quantity: 20
+    },
+    {
+        id: 7,
+        name: "芒果綠茶",
+        description: "芒果與綠茶的獨特風味",
+        price: 55,
+        quantity: 18
+    },
+    {
+        id: 8,
+        name: "抹茶拿鐵",
+        description: "抹茶與鮮奶的絕配",
+        price: 60,
+        quantity: 20
+    },
+]
+
+const First = () => {
+
+    const [cup, setCup] = useState(data);
+    const [edit, setEdit] = useState(false);
+    const [category, setCategory] = useState('品項');
+
+    const handleMinus = (itemId) => {
+        setCup((prevCup) =>
+            prevCup.map((item) =>
+                item.id === itemId
+                    ? { ...item, quantity: Math.max(0, item.quantity - 1) }
+                    : item
+            )
+        );
+    };
+
+    const handleAdd = (itemId) => {
+        setCup((prevCup) =>
+            prevCup.map((item) =>
+                item.id === itemId
+                    ? { ...item, quantity: item.quantity + 1 }
+                    : item
+            )
+        );
+    };
+
+    return (
+        <>
+            <table>
+                <thead>
+                    <tr>
+                        <th scope="col">品項</th>
+                        <th scope="col">描述</th>
+                        <th scope="col">價格</th>
+                        <th scope="col">庫存</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        cup.map((item) => {
+                            return (
+                                <tr key={item.id}>
+                                    <td>{item.name}</td>
+                                    <td><small>{item.description}</small></td>
+                                    <td>{item.price}</td>
+                                    <td>
+                                        <button onClick={() => handleMinus(item.id)}>-</button>
+                                        {item.quantity}
+                                        <button onClick={() => handleAdd(item.id)}>+</button>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table >
+        </>
+    )
+}
+export default First;
